c60be032c1158a2739c4e8f3fe98a054b61cc983
Annie Hawes <annie.hawes@nationalarchives.gov.uk>
2023-10-04 11:15:54 +0100

    Switch from receiving messages from tre internal to da eventbus

--- variables.tf
+++ variables.tf
@@ -18,8 +18,8 @@ variable "tre_permission_boundary_arn" {
   type = string
 }
 
-variable "tre_internal_topic_arn" {
-  description = "ARN of the tre-out sns topic"
+variable "da_eventbus_topic_arn" {
+  description = "ARN of the da-eventbus sns topic"
   type = string
 }
 
